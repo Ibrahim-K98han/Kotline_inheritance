1c6ac5858ede26dcee486ffcbfde9f7883d04627
Ibrahim Khan <aitvetibrahim@gmail.com>
2022-07-07 07:56:34 +0600

    Object create in main class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,14 @@
+import model.BaseSalaryEmployee
+import model.ComissionSalaryEmployee
+import model.HourlySalaryEmployee
+
 fun main() {
 
+    val bse = BaseSalaryEmployee("Ibrahim",49L,1000.0)
+    val hse = HourlySalaryEmployee("Hasan",45L,10,50)
+    val cse = ComissionSalaryEmployee("Tomal",78L,10000.0,1000.0)
+
+    println(bse.calculateSalary())
+    println(hse.calculateSalary())
+    println(cse.calculateSalary())
 }
--- src/main/kotlin/model/ComissionSalaryEmployee.kt
+++ src/main/kotlin/model/ComissionSalaryEmployee.kt
@@ -1,5 +1,12 @@
 package model
 
-class ComissionSalaryEmployee {
+class ComissionSalaryEmployee(
+    name:String,
+    id:Long,
+    val salary:Double,
+    val totalSale:Double,
+    val commissionRate:Double = 2.5
+):Employee(name,id) {
+    override fun calculateSalary() = salary + (totalSale * commissionRate)/100
 
 }
